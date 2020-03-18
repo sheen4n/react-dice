8fd721671559103cf6324c71930c5cd339ce5b80
Goh Sheen An <sheen.an.goh@gmail.com>
2020-03-18 10:49:42 +0800

    Updates

---
+++ precache-manifest.9d00ac8beda959394bfa357a286919b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21c153a9a274c24242cf41530bd09330",
+    "url": "/react-dice/index.html"
+  },
+  {
+    "revision": "e5bb3e2ecc680966d9ec",
+    "url": "/react-dice/static/css/main.b22b4224.chunk.css"
+  },
+  {
+    "revision": "3044e3035d0f9bf0acab",
+    "url": "/react-dice/static/js/2.93325f7b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-dice/static/js/2.93325f7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5bb3e2ecc680966d9ec",
+    "url": "/react-dice/static/js/main.4e4383ab.chunk.js"
+  },
+  {
+    "revision": "3c962bc8c981d2479b90",
+    "url": "/react-dice/static/js/runtime-main.8a501bee.js"
+  }
+]);
